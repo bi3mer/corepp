1780f3c25c7f543097e3954eb73fdd35feada0a5
bi3mer <colan@comcast.net>
2025-07-31 09:44:25 -0400

    renamed int types

--- src/types.hpp
+++ src/types.hpp
@@ -2,10 +2,10 @@
 
 #include <cstdint>
 
-typedef int8_t s8;
-typedef int16_t s16;
-typedef int32_t s32;
-typedef int64_t s64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
 
 typedef uint8_t u8;
 typedef uint16_t u16;
