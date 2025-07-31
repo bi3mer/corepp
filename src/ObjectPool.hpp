f5d794c170e53c3e271003b665b1636df34704ee
bi3mer <colan@comcast.net>
2025-07-31 16:05:28 -0400

    added ObjectPool and PooledLinkedList

---
+++ src/ObjectPool.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <cstddef>
+#include <vector>
+
+#include "log.hpp"
+
+template<typename T>
+struct ObjectPool {
+    std::vector<T*> pool;
+
+    ObjectPool() {};
+
+    ObjectPool(std::size_t initial_pool_size)  {
+       allocate_objects(initial_pool_size);
+    }
+
+    ~ObjectPool() {
+        for (T* t : pool) {
+            delete t;
+        }
+    }
+
+    void allocate_objects(std::size_t size) {
+        for (std::size_t i = 0; i < size; ++i) {
+            pool.push_back(new T());
+        }
+    }
+
+    T* get_object() {
+        if (pool.empty()) {
+            Log::debug("pool :: alloc");
+            return new T();
+        }
+
+        Log::debug("pool :: get");
+        T* object = pool.back();
+        pool.pop_back();
+
+        return object;
+    }
+
+    void return_object(T* object) {
+        pool.push_back(object);
+    }
+};
